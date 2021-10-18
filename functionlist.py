1fec05ccd5800ce517878719184d50388af097d6
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-18 08:00:41 -0700

    Update functionlist.py

--- functionlist.py
+++ functionlist.py
@@ -1,8 +1,4 @@
 ##CALLABLE COMMANDS
-
-def pingcommand():
-    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-
 def helprender(jsondata):
     return str(jsondata["commands"].keys()).replace("dict_keys(","").replace("])","]")
     
